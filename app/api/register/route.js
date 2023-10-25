00ae4c9c7250982ea56ada2217ab132b176e44e1
Mello Birkan <mgbirkan@icloud.com>
2023-10-25 16:49:59 -0300

    `Conexção com banco de dados` Finalizada

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,11 +1,15 @@
 import {NextResponse} from "next/server";
+import User from "@/models/user";
+import { connectMongoDB } from "@/lib/mongodb"
+import bcrypt from "bcryptjs"
 
 export async function POST(req) {
   try {
     const {name, email, password} = await req.json();
-    console.log("Name ", name)
-    console.log("E-mail ", email)
-    console.log("Password ", password)
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await connectMongoDB();
+    await User.create({ name, email, password: hashedPassword });
+
     return NextResponse.json({message: "User registered."}, { status: 201});
   } catch (error) {
     return NextResponse.json({message: "Error ocurred while registering the user."}, {status: 500})
